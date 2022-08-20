dc33e441941b1b95c1dcd903b9d1a1f698be55f0
romanboyarkin <boyra75.rb@gmail.com>
2022-08-20 12:29:05 +0900

    java stepik

---
+++ src/lesson1/MyClass.java
@@ -0,0 +1,10 @@
+package lesson1;
+
+//import java.lang.*;
+
+public class MyClass extends Object {
+
+    public static void main(java.lang.String[] args) {
+        System.err.println("Hello world");
+    }
+}
